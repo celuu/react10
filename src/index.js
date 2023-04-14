bb61bb11f8058007182e026d5e39631adb100c29
celuu <christine.e.luu@gmail.com>
2023-04-13 18:00:49 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import configureStore from "./store";
+import { Provider} from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+const store = configureStore()
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +20,21 @@ root.render(
   </React.StrictMode>
 );
 
+function Root() {
+  return (
+    <div className="index-background">
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
+}
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
