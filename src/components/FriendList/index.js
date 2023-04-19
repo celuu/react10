a8a7681bd3785e3c7370d02aa1206d3b888a88d6
celuu <christine.e.luu@gmail.com>
2023-04-18 17:01:53 -0700

    Adding filter

--- src/components/FriendList/index.js
+++ src/components/FriendList/index.js
@@ -1,37 +1,46 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import "./Friend.css"
 
 
 const FriendList = () => {
 
+    const [filterClicked, setFilterClicked] = useState(false)
+    const [filter, setFilter] = useState("")
     const [list, setList] = useState([
       {
         name: "Christine",
         checked: false,
+        age: 25
       },
       {
         name: "Jacob",
         checked: false,
+        age: 35
       },
       {
         name: "Morgan",
         checked: false,
+        age: 6
       },
       {
         name: "Nishant",
         checked: false,
+        age: 21
       },
       {
         name: "Kaushal",
         checked: false,
+        age: 45
       },
       {
         name: "Ashley",
         checked: false,
+        age: 60
       },
       {
         name: "Calli",
         checked: false,
+        age: 35
       }
     ]);
 
@@ -39,20 +48,18 @@ const FriendList = () => {
 
     const selectAllClickHandler = (e) => {
         if(!e.target.checked) return;
-        const newList = list;
-        newList.forEach((person) => {
+        list.forEach((person) => {
           person.checked = true;
         });
-        setList([...newList]);
+        setList([...list]);
     }
 
     const deselectAllClickHandler = (e) => {
         if(!e.target.checked) return;
-        const newList = list;
-        newList.forEach((person) => {
+        list.forEach((person) => {
           person.checked = false;
         });
-        setList([...newList]);
+        setList([...list]);
     }
 
     const clickCheckboxHandler = (e, idx) => {
@@ -60,27 +67,95 @@ const FriendList = () => {
         setList([...list])
     }
 
+    const filterAgeClickHandler = (e, idx) => {
+        if(e.target.id === "below-21") {
+            list.map((person) => {
+                person.age < 21
+            })
+            return list
+        } else if (e.target.id === "21-25") {
+        } else if (e.target.id === "25-35") {
+        } else {
+        }
+    }
+
+
     return (
-      <div className="friend-container">
-        <label>
-          <input type="checkbox" onClick={e => selectAllClickHandler(e)}></input>
-          Select All
-        </label>
-        <label>
-          <input type="checkbox" onClick={e => deselectAllClickHandler(e)}></input>
-          Deselect All
-        </label>
-        {list.map((person, idx) => {
-            console.log(person);
-          return (
-            <>
-              <label className="friend-labels">
-                <input type="checkbox" key={idx} name={person.name} id={idx} checked={person.checked} onChange={(e) => clickCheckboxHandler(e, idx)} />
-                {person.name}
-              </label>
-            </>
-          );
-        })}
+      <div className="total-container">
+        <div className="all-container">
+          <label className="select-all">
+            <input
+              type="checkbox"
+              onClick={(e) => selectAllClickHandler(e)}
+            ></input>
+            Select All
+          </label>
+          <label className="deselect-all">
+            <input
+              type="checkbox"
+              onClick={(e) => deselectAllClickHandler(e)}
+            ></input>
+            Deselect All
+          </label>
+          <div className="fiter-dropdown">
+            <button
+              className="age-button"
+              onClick={(e) => setFilterClicked((prev) => !prev)}
+            >
+              Filter By Age
+            </button>
+            <div className={filterClicked ? "age-options" : "no-age-options"}>
+              <li
+                className="each-option"
+                id="below-21"
+                onClick={filterAgeClickHandler}
+              >
+                5 - 21
+              </li>
+              <li
+                className="each-option"
+                id="21-25"
+                onClick={filterAgeClickHandler}
+              >
+                21 -25
+              </li>
+              <li
+                className="each-option"
+                id="25-35"
+                onClick={filterAgeClickHandler}
+              >
+                25 - 35
+              </li>
+              <li
+                className="each-option"
+                id="35plus"
+                onClick={filterAgeClickHandler}
+              >
+                35 +
+              </li>
+            </div>
+          </div>
+        </div>
+
+        <div className="friend-container">
+          {list.map((person, idx) => {
+            return (
+              <>
+                <label className="friend-labels">
+                  <input
+                    type="checkbox"
+                    key={idx}
+                    name={person.name}
+                    id={idx}
+                    checked={person.checked}
+                    onChange={(e) => clickCheckboxHandler(e, idx)}
+                  />
+                  {person.name}
+                </label>
+              </>
+            );
+          })}
+        </div>
       </div>
     );
 }
